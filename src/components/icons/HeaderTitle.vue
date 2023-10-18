68874b97598cfd9b03d9c1287b4a001d3b704508
FanilZarip <fanilfan1994@mail.ru>
2023-10-18 07:50:17 +0300

    Добавил заголовок

---
+++ src/components/icons/HeaderTitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="header-title">VUE3 Simple Tooltip</p>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header-title {
+  font-size: 32px;
+  text-align: center;
+  text-transform: uppercase;
+}
+</style>
