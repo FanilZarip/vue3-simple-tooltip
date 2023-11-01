f78c9c3d8c186878e33cea8ccbd5be8aea02a4d9
FanilZarip <fanilfan1994@mail.ru>
2023-11-01 07:22:00 +0300

    Добавил типы для кнопки, скорректировал вертску

--- src/components/SButton.vue
+++ src/components/SButton.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="button">
+  <button class="button" :class="{ 'button-radial': type === 'radial' }">
     <slot name="prepend-icon" />
-    <button v-bind="$attrs">
+    <span>
       {{ text }}
-    </button>
+    </span>
     <slot name="append-icon" />
-  </div>
+  </button>
 </template>
 <script setup lang="ts">
-defineOptions({
-  inheritAttrs: false,
-});
+type buttonType = "radial" | "default";
 
 interface Button {
   text: string;
-  backgroundColor: string;
-  color: string;
+  backgroundColor?: string;
+  color?: string;
+  type: buttonType;
 }
 
-const props = withDefaults(defineProps<Button>(), {
+withDefaults(defineProps<Button>(), {
   backgroundColor: "#1a73e8",
   color: "white",
 });
@@ -28,14 +27,29 @@ const props = withDefaults(defineProps<Button>(), {
 
 <style scoped>
 .button {
-  background-color: v-bind("props.backgroundColor");
-  display: inline-flex;
+  background-color: v-bind("backgroundColor");
+
   padding: 10px 30px;
+
+  border: none;
+  color: v-bind("color");
+
+  cursor: pointer;
 }
 
-.button > button {
-  border: none;
-  background: none;
-  color: v-bind("props.color");
+.button:hover {
+  filter: brightness(1.2);
+}
+
+.button:active {
+  filter: brightness(0.9);
+}
+
+.button-radial {
+  border-radius: 4px;
+}
+
+.button > span {
+  padding: 0 10px;
 }
 </style>
